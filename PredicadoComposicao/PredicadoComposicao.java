e6d9ff1f789885afc38d312ab06675a5e38df942
Lucas de Lima Martinez <lucasmartinez.aluno@unipampa.edu.br>
2023-08-08 12:59:48 -0300

    Predicado e composicão

---
+++ PredicadoComposicao/PredicadoComposicao.java
@@ -0,0 +1,13 @@
+package PredicadoComposicao;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+    public static void main(String[] args) {
+        
+        Predicate<Integer> isPar = num -> num % 2 ==0;
+        Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+
+        System.out.println(isTresDigitos.and(isPar).test(997));
+    }
+}
